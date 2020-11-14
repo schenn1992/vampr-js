05377fc1ea04c760d4612580b7c4ba39afb8d5de
Steven Chen <schen@Stevens-MacBook-Pro.local>
2020-11-14 12:45:16 -0500

    complete all funcs

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampire = 0;
+    let currentVampire = this;
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampire ++;
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.yearConverted < vampire.yearConverted;
+    
   }
 
   /** Stretch **/
@@ -39,6 +47,10 @@ class Vampire {
 
   }
 }
+
+const steve = new Vampire()
+
+
 
 module.exports = Vampire;
 
